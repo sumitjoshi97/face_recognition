cc737524c6d174e9add7c290284bfc3ffc15f387
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-04-30 12:37:21 +0530

    repo initialized

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navigation = (props) => (
+    <nav style={{display: 'flex', justifyContent: 'flex-end', padding:'30px'}}>
+        <p>Sign out</p>
+    </nav>
+);
+
+export default Navigation;
