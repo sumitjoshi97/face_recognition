f7d0e0102042b755e314c3fb240f0cdc1a93fa15
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-04-30 12:56:56 +0530

    added logo

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+const Logo = (props) => (
+    <div className="m4 mt0">
+        <Tilt
+            className="Tilt"
+            options={{
+            max: 25
+        }}
+            style={{
+            height: 250,
+            width: 250
+        }}>
+            <div className="Tilt-inner">
+                👽
+            </div>
+        </Tilt>
+    </div>
+);
+
+export default Logo;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navigation = (props) => (
     <nav style={{display: 'flex', justifyContent: 'flex-end', padding:'30px'}}>
-        <p>Sign out</p>
+        <p className="f3 link dim black underline pa3 pointer">Sign out</p>
     </nav>
 );
 
