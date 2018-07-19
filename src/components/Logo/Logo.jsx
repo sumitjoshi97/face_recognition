861b11c666d1ceca9f5884cec1792674c8768ccb
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-07-19 17:50:21 +0530

    logo component modified

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import './Logo.css';
+import brain from './brain.png'
 const Logo = (props) => (
     <div className="m4 mt0">
         <Tilt
-            className="Tilt"
+            className="Tilt Logo"
             options={{
             max: 25
         }}
             style={{
-            height: 250,
-            width: 250
+            height: 150,
+            width: 150
         }}>
             <div className="Tilt-inner">
-                👽
+                <img style={{paddingTop: '10px'}}src={brain} alt="logo"/>
             </div>
         </Tilt>
     </div>
