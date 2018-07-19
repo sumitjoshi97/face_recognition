416edd448fb35fdb23996439e7eee097d32fd7b9
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-07-19 17:49:50 +0530

    show rank of image

---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Rank extends Component {
+  render() {
+    return (
+      <div>
+        <div className="white f3">
+            {`Your current rank is`}
+            <h2>#5</h2>
+        </div>
+      </div>
+    )
+  }
+}
